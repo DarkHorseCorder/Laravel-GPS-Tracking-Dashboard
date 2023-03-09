6be932807287c6ecd92ccbf160e327a5e07b81d5
DarkHorseCoder <120799088+DarkHorseCorder@users.noreply.github.com>
2023-03-09 18:13:46 +0900

    remove action from green driving form remove green driving report generate function from controller remove green driving report generate route

--- resources/views/pages/green-driving.blade.php
+++ resources/views/pages/green-driving.blade.php
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
-                    <form role="form" id="greenDrivingForm" method="POST" action={{ route('generateReport.green-driving') }} enctype="multipart/form-data">
+                    <form role="form" id="greenDrivingForm" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="card-body">
                             <div class="row">
